18b6dab7f5d56abfc526ccd0e4e8ad8bda20bea1
Dmitrenko <Dias64@mail.ru>
2019-10-10 16:35:40 +0300

    Step 1

---
+++ src/main/java/ru/dias/hibernate/crud/CrudApp.java
@@ -0,0 +1,54 @@
+package ru.dias.hibernate.crud;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import ru.dias.hibernate.validation.PrepareDataApp;
+
+public class CrudApp {
+    public static void main(String[] args) throws Exception {
+        PrepareDataApp.forcePrepareData();
+
+        Long maxId = null;
+
+        SessionFactory factory = new Configuration()
+                .configure("resources/hibernate.cfg.xml")
+                .buildSessionFactory();
+
+        Session session = null;
+        try {
+            // запись
+            session = factory.getCurrentSession();
+            session.beginTransaction();
+            SimpleItem newSimpleItem = new SimpleItem("Chair", 1000);
+            session.save(newSimpleItem);
+            session.getTransaction().commit();
+
+            // чтение по id = 1
+            session = factory.getCurrentSession();
+            session.beginTransaction();
+            SimpleItem simpleItemFromDb = session.get(SimpleItem.class, 1L);
+            session.getTransaction().commit();
+
+            // изменение
+            session = factory.getCurrentSession();
+            session.beginTransaction();
+            maxId = session.createQuery("SELECT MAX(s.id) FROM SimpleItem s", Long.class).getSingleResult();
+            SimpleItem simpleItemForUpdate = session.createQuery("SELECT s FROM SimpleItem s WHERE s.id = :id", SimpleItem.class)
+                    .setParameter("id", maxId)
+                    .getSingleResult();
+            simpleItemForUpdate.setPrice(simpleItemForUpdate.getPrice() + 100);
+            session.getTransaction().commit();
+
+            // удаление
+            session = factory.getCurrentSession();
+            session.beginTransaction();
+            maxId = session.createQuery("SELECT MAX(s.id) FROM SimpleItem s", Long.class).getSingleResult();
+            session.delete(session.get(SimpleItem.class, maxId));
+            session.getTransaction().commit();
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+}
---
+++ src/test/full.sql
@@ -0,0 +1,8 @@
+DROP TABLE IF EXISTS simple_items CASCADE;
+CREATE TABLE simple_items (id bigserial PRIMARY KEY, title VARCHAR(255), price int);
+INSERT INTO simple_items (title, price) VALUES
+('box', 10),
+('sphere', 20),
+('maul', 100),
+('door', 500),
+('camera', 1000);
